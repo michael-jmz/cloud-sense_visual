b1cdeffa9ce8d06d40e17c8a0109cb4e3eace06a
michael-jmz <www.michaelj@hotmail.es>
2024-06-11 23:57:02 -0500

    configuration angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MaterialModule } from './modules/material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Angular Material
+import {MatCardModule} from '@angular/material/card';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
+  ],
+  exports: [
+    MatCardModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
+
+  ]
+})
+export class MaterialModule { }
